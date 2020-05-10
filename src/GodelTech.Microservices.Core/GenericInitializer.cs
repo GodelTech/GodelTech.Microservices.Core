46d0838bda60845a1bde747bf0658619d16b2054
Andrei Salanoi <diamond_dragon@tut.by>
2020-05-11 00:09:59 +0300

    Refactoring (#21)

---
+++ src/GodelTech.Microservices.Core/GenericInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GodelTech.Microservices.Core
+{
+    public class GenericInitializer : IMicroserviceInitializer
+    {
+        private readonly Action<IApplicationBuilder, IWebHostEnvironment> _configureAction;
+        private readonly Action<IServiceCollection> _configureServices;
+
+        public GenericInitializer(
+            Action<IApplicationBuilder, IWebHostEnvironment> configureAction = null,
+            Action<IServiceCollection> configureServices = null)
+        {
+            _configureAction = configureAction;
+            _configureServices = configureServices;
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            _configureAction?.Invoke(app, env);
+
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            _configureServices?.Invoke(services);
+        }
+    }
+}
