d5efa1a43dda408773665d703028ab8c9bf72a1a
goldenstar <golden_star@tut.by>
2020-08-25 12:38:18 +0300

    Remove unusable "using"

--- src/GodelTech.Microservices.Core/MicroserviceStartup.cs
+++ src/GodelTech.Microservices.Core/MicroserviceStartup.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using GodelTech.Microservices.Core.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
