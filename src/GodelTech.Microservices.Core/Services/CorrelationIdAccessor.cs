ca49ceb139b14eb5530d2319dd92fd8c226d3e95
Andrei Salanoi <diamond_dragon@tut.by>
2021-04-04 16:41:28 +0300

    Minor clean up (#51)
    Minor clean up

--- src/GodelTech.Microservices.Core/Services/CorrelationIdAccessor.cs
+++ src/GodelTech.Microservices.Core/Services/CorrelationIdAccessor.cs
@@ -3,9 +3,6 @@ using System.Threading;
 
 namespace GodelTech.Microservices.Core.Services
 {
-    /// <summary>
-    /// This class is supposed to be used by Intelliflo.Platform only
-    /// </summary>
     public sealed class CorrelationIdAccessor : ICorrelationIdAccessor, ICorrelationIdSetter
     {
         private static readonly AsyncLocal<string> CorrelationIdStorage = new AsyncLocal<string>();
