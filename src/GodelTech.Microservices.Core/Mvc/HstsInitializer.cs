9aaac5e0d23c80314dd93efddb684d59da6e4489
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-09-17 17:47:33 +0300

    HSTS initializer added

---
+++ src/GodelTech.Microservices.Core/Mvc/HstsInitializer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GodelTech.Microservices.Core.Mvc
+{
+    /// <summary>
+    /// HSTS Initializer
+    /// </summary>
+    public class HstsInitializer : IMicroserviceInitializer
+    {
+        /// <inheritdoc />
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        /// <inheritdoc />
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+        }
+    }
+}
