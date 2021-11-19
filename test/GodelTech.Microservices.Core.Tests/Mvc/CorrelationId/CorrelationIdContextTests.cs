3982d21401950ecffa6b2e1e7da818c844499d59
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-11-19 17:03:36 +0300

    add CorrelationIdContextTests

---
+++ test/GodelTech.Microservices.Core.Tests/Mvc/CorrelationId/CorrelationIdContextTests.cs
@@ -0,0 +1,20 @@
+﻿using GodelTech.Microservices.Core.Mvc.CorrelationId;
+using Xunit;
+
+namespace GodelTech.Microservices.Core.Tests.Mvc.CorrelationId
+{
+    public class CorrelationIdContextTests
+    {
+        [Fact]
+        public void CorrelationId_Get_Success()
+        {
+            // Arrange
+            const string expectedCorrelationId = "TestCorrelationId";
+
+            var context = new CorrelationIdContext(expectedCorrelationId);
+
+            // Act & Assert
+            Assert.Equal(expectedCorrelationId, context.CorrelationId);
+        }
+    }
+}
