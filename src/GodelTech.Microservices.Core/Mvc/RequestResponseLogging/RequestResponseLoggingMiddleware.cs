c63e78ed8e6e7fc0e9a2b5868f6149f12725e684
Vladimir Rodchenko <vladimir@rodchenko.by>
2022-01-03 15:14:55 +0300

    add init RequestResponseLoggingMiddleware

---
+++ src/GodelTech.Microservices.Core/Mvc/RequestResponseLogging/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace GodelTech.Microservices.Core.Mvc.RequestResponseLogging
+{
+    /// <summary>
+    /// Middleware which logs requests and responses.
+    /// </summary>
+    public class RequestResponseLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestResponseLoggingMiddleware> _logger;
+
+        /// <summary>
+        /// Creates a new instance of the RequestResponseLoggingMiddleware.
+        /// </summary>
+        /// <param name="next">The next middleware in the pipeline.</param>
+        /// <param name="logger">The logger.</param>
+        public RequestResponseLoggingMiddleware(
+            RequestDelegate next,
+            ILogger<RequestResponseLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Processes a request.
+        /// </summary>
+        /// <param name="context">The <see cref="HttpContext"/> for the current request.</param>
+        public Task InvokeAsync(HttpContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            return InvokeInternalAsync(context);
+        }
+
+        private async Task InvokeInternalAsync(HttpContext context)
+        {
+            // code dealing with the request
+
+            await _next(context);
+
+            // code dealing with the response
+        }
+    }
+}
---
+++ src/GodelTech.Microservices.Core/Mvc/RequestResponseLoggingMiddlewareInitializer.cs
@@ -0,0 +1,25 @@
+﻿using GodelTech.Microservices.Core.Mvc.RequestResponseLogging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GodelTech.Microservices.Core.Mvc
+{
+    /// <summary>
+    /// RequestResponseLoggingMiddleware initializer.
+    /// </summary>
+    public class RequestResponseLoggingMiddlewareInitializer : IMicroserviceInitializer
+    {
+        /// <inheritdoc />
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        /// <inheritdoc />
+        public virtual void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<RequestResponseLoggingMiddleware>();
+        }
+    }
+}
