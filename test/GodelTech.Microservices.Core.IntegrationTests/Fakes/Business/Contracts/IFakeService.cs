b1b2da5dc6f1589c63abbaa79b9d81dd8e43e175
Vladimir Rodchenko <vladimir@rodchenko.by>
2022-07-06 19:17:20 +0300

    #pragma warning disable CA1716 // Identifiers should not match keywords

--- test/GodelTech.Microservices.Core.IntegrationTests/Fakes/Business/Contracts/IFakeService.cs
+++ test/GodelTech.Microservices.Core.IntegrationTests/Fakes/Business/Contracts/IFakeService.cs
@@ -8,7 +8,9 @@ namespace GodelTech.Microservices.Core.IntegrationTests.Fakes.Business.Contracts
     {
         IList<FakeDto> GetList();
 
+#pragma warning disable CA1716 // Identifiers should not match keywords
         FakeDto Get(int id);
+#pragma warning restore CA1716 // Identifiers should not match keywords
 
         FakeDto Add(IFakeAddDto item);
 
